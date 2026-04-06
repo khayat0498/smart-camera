719712e19e6c4bcd79bdb8b7144c3a4d390a8dad
khayat0498 <hayotbek040498@gmail.com>
2026-04-07 00:36:37 +0500

    first update smart camera link with opencv4

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages deploy uchun
-  base: process.env.NODE_ENV === 'production' ? '/smart-camera/' : '/',
+  base: './',  // ✅ Eng ishonchli usul
   server: {
     port: 3000,
     open: true
